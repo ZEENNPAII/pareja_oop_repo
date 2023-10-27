7ab27b66b2e25fd2f8b94ee8fdaf476fcf08b06b
ZEENNPAII <112284969+ZEENNPAII@users.noreply.github.com>
2023-10-27 11:45:11 +0800

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,6 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        String message = "Sa pagsapit ng dilim\nAko'y naghihintay pa rin\nSa iyong maagang pagdating\n'Pagkat ako'y nababalisa\nKung 'di ka kapiling\nBawat sandali'y mahalaga sa atin";
+        System.out.println(message);
+    }
+}
